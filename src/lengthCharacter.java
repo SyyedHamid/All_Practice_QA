6943d78a1de83749a226d3203bd39ed0537c5e4e
SyyedHamid <sayedmdkowserhamid@gmail.com>
2024-09-09 15:45:07 -0400

    lengthCharacter class

---
+++ src/lengthCharacter.java
@@ -0,0 +1,16 @@
+public class lengthCharacter {
+
+    public static void main(String[] args) {
+        String x= "We Learning Java";
+        System.out.println("The vale of x is: "+x);
+
+        //Getting value in lowercase
+        System.out.println("The vale of x with lower case: "+x.toLowerCase());
+        //Getting value in uppercase
+        System.out.println("The vale of x with upper case: "+x.toUpperCase());
+
+        //Calculating length
+        System.out.println("The length of the Above String is- : "+x.length());
+    }
+}
+
