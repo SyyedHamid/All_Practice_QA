e574eeb4acc06ac1203df2f5d61b7760705d6171
SyyedHamid <sayedmdkowserhamid@gmail.com>
2024-09-10 10:30:27 -0400

    Added subSring, charAt,Concat,Contains

--- src/lengthCharacter.java
+++ src/lengthCharacter.java
@@ -8,9 +8,24 @@ public class lengthCharacter {
         System.out.println("The vale of x with lower case: "+x.toLowerCase());
         //Getting value in uppercase
         System.out.println("The vale of x with upper case: "+x.toUpperCase());
+        //Ignore Case Sensitive
+        System.out.println("Comparing with x value- "+x.equalsIgnoreCase("We Learning Java"));
+
 
         //Calculating length
         System.out.println("The length of the Above String is- : "+x.length());
+
+        //To Check any indexNumber character  from the String Value
+        System.out.println("Above String value contains "+x.charAt(4));
+        //To check any character from the above String value
+        System.out.println("Above String value contains "+x.contains("a"));
+        //Concat any value
+        System.out.println("Student ID- ".concat("16029-669-09"));
+
+        //SubString help to split any String value
+        System.out.println("After splitting by using Substring- "+x.substring(4));
+        //Using Beginning  and Ending Index
+        System.out.println("After splitting by using Substring- "+x.substring(1,5));
     }
 }
 
